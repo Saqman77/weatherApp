d434dfb6e8182d2eca52ee90802417b2dd0a92a8
Saqman77 <saqlainhaider77@gmail.com>
2024-12-31 01:06:30 +0500

    card

--- weather-tsx/src/components/cards/Card.tsx
+++ weather-tsx/src/components/cards/Card.tsx
@@ -2,7 +2,24 @@ import './cards.scss'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='Cardcontainer'>
+        <div className='card'>
+            <div className='card-header'>
+            <h2>Monday</h2>
+            <p>12:30pm</p>
+            </div>
+            <div className='card-body'>
+            <div className='temp'>
+                <h2>Temperature</h2>
+                <p>°C</p>
+            </div>
+            <div className='weather'>
+                <h2>Weather</h2>
+                <p>Weather Description</p>
+            </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
