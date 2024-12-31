e8e8de0ee3268ee9fa5d967fafb81f8ee64c0262
Saqman77 <saqlainhaider77@gmail.com>
2025-01-01 02:03:27 +0500

    index circle

--- weather-tsx/src/components/main/Right-column.tsx
+++ weather-tsx/src/components/main/Right-column.tsx
@@ -7,7 +7,8 @@ const RightColumn = () => {
     }
 
   return (
-    <div>
+    <div className="uv-container">
+    <div className='uv-wrapper'>
         <div className="uv">
             <div className="outer">
                 <div className="inner">
@@ -17,7 +18,17 @@ const RightColumn = () => {
                 </div>
             </div>
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+         </defs>
+         <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+        </svg>
     </div>
+</div>
   )
 }
 
