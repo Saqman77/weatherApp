d01a88dd176c8d2f0399a86ed195d24c23043e1d
Saqman77 <saqlainhaider77@gmail.com>
2025-01-01 05:36:52 +0500

    box shadows added

--- weather-tsx/src/components/header/Header.tsx
+++ weather-tsx/src/components/header/Header.tsx
@@ -1,12 +1,38 @@
 import './header.scss'
-
+import logo from '../../assets/icons/football-grass-green-nature-soccer-svgrepo-com.svg'
+import searchIcon from '../../assets/icons/search-svgrepo-com.svg'
 const Header = () => {
   return (
     <>
     <div className='header'>
+      <div className="logo"
+        style={{
+          width: '30px',
+          height: '30px',
+        }}
+      >
+        <img
+            style={{
+              width: '30px',
+              height: '30px',
+            }}
+        src={logo} alt="" />
+      </div>
       <div className="search-wrapper">
         <input type="text" placeholder="Search for city" className='search' />
-        <button>s</button>
+        <button
+        style={{
+          width: '40px',
+          height: '40px',
+          // backgroundColor: 'transparent',
+        }}
+        ><img 
+        style={{
+          // width: '30px',
+          // height: '30px',
+          // zIndex: 1,
+        }}
+        src={searchIcon} alt="" className='searchImg'/></button>
       </div>
       <div className="toggle">
         <label className="switch">
